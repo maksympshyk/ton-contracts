8003a637ad35e5a4575f8cacc1017739785e636f
maksympshyk <pshykmaksym@gmail.com>
2024-05-08 21:30:54 +0200

    add 2 methods to contract wrapper, extend test spec

--- contract_one/tests/main.spec.ts
+++ contract_one/tests/main.spec.ts
@@ -1,3 +1,20 @@
+import { Cell, toNano } from "@ton/core";
+import { hex } from "../build/main.compiled.json";
+import { Blockchain } from "@ton-community/sandbox";
+import { MainContract } from "../wrappers/MainContract";
+
 describe("main.fc contract tests", () => {
-  it("our first test", async () => {});
+  it("our first test", async () => {
+    const blockchain = await Blockchain.create();
+
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig({}, codeCell)
+    );
+
+    const senderWallet = await blockchain.treasury("sender");
+
+    myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"));
+  });
 });
--- contract_one/wrappers/MainContract.ts
+++ contract_one/wrappers/MainContract.ts
@@ -1,4 +1,13 @@
-import { Address, beginCell, Cell, Contract, contractAddress } from "@ton/core";
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+} from "@ton/core";
 
 export class MainContract implements Contract {
   constructor(
@@ -12,5 +21,24 @@ export class MainContract implements Contract {
     const address = contractAddress(workchain, init);
 
     return new MainContract(address, init);
+  }
+
+  async sendInternalMessage(
+    provider: ContractProvider,
+    sender: Sender,
+    value: bigint
+  ) {
+    await provider.internal(sender, {
+      value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell().endCell(),
+    });
+  }
+
+  async getData(provider: ContractProvider) {
+    const { stack } = await provider.get("get_the_last_sender", []);
+    return {
+      recent_sender: stack.readAddress(),
+    };
   }
 }
